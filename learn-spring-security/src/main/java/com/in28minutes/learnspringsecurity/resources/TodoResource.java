ce49908eb4fbfd93d7ecd8be725ca77f1ef25276
vaibhav7879 <84507753+vaibhav7879@users.noreply.github.com>
2023-03-28 12:58:30 +0530

    Add files via upload

---
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/resources/TodoResource.java
@@ -0,0 +1,33 @@
+package com.in28minutes.learnspringsecurity.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public static final List<Todo> TODOS_LIST = List.of(new Todo("in28minutes", "Learn AWS"),
+            new Todo("in28minutes", "Get AWS Certified"));
+
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos(){
+        return TODOS_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveTodosForSpecificUser(@PathVariable String username){
+        return TODOS_LIST.get(0);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public void createTodosForSpecificUser(@PathVariable String username, @RequestBody Todo todo){
+        logger.info("Create {} for {}",todo,username);
+    }
+}
+
+record Todo(String username,String description){}
